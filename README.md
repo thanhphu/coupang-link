200e66c0a00e1a29c279fa7966b445abf5fbd772
Thanh Phu <git@thanhphu.net>
2021-07-15 03:07:48 +0900

    Add URL expanding function

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Coupang link generator
 
 Demo
+
+
 ![](docs/demo.png)
 
-* Fill in your keys
-* Upload to server
-* Click submit request
+- Fill in your keys in `config.php`
+```
+  // Replace with your own ACCESS_KEY and SECRET_KEY
+  $ACCESS_KEY = "xxx";
+  $SECRET_KEY = "xxx";
+```
+- Upload to server
+- Click submit request
+
+## Features
+- Automatically resolve affiliate links and convert them to yours
--- request.php
+++ request.php
@@ -1,9 +1,56 @@
 <?php
+
+function http_parse_query($query)
+{
+  $parameters = array();
+  $queryParts = explode('&', $query);
+  foreach ($queryParts as $queryPart) {
+    $keyValue = explode('=', $queryPart, 2);
+    if ($keyValue[0] == "itemId" || $keyValue[0] == "vendorItemId") {
+      $parameters[$keyValue[0]] = $keyValue[1];
+    }
+  }
+  return $parameters;
+}
+
+function build_url(array $parts)
+{
+  return (isset($parts['scheme']) ? "{$parts['scheme']}:" : '') .
+    ((isset($parts['user']) || isset($parts['host'])) ? '//' : '') .
+    (isset($parts['user']) ? "{$parts['user']}" : '') .
+    (isset($parts['pass']) ? ":{$parts['pass']}" : '') .
+    (isset($parts['user']) ? '@' : '') .
+    (isset($parts['host']) ? "{$parts['host']}" : '') .
+    (isset($parts['port']) ? ":{$parts['port']}" : '') .
+    (isset($parts['path']) ? "{$parts['path']}" : '') .
+    (isset($parts['query']) ? "?{$parts['query']}" : '') .
+    (isset($parts['fragment']) ? "#{$parts['fragment']}" : '');
+}
+
+function expand_url($url) {
+  $expanded_url = $url;
+  if (strpos($url, "coupa.ng") !== false) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Must be set to true so that PHP follows any "Location:" header
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $a = curl_exec($ch); // $a will contain all headers
+
+    $expanded_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
+
+    $parts = parse_url($expanded_url);
+    $expanded_url = build_url($parts);
+  }
+  return $expanded_url;
+}
+
 if ($_POST["urls"]) {
   $lines = explode("\n", $_POST['urls']);
   $quotedLines = array();
   foreach ($lines as &$line) {
-    $line = trim($line);
+    $line = expand_url(trim($line));
     $quotedLines[] = "\"${line}\"";
   }
   $coupangUrls = implode(",", $quotedLines);
